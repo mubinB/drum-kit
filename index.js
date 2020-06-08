cdfa77cd6b30a0490fcba86c6274a6ba642c2ea1
mubin bagwan <mubinB@users.noreply.github.com>
2020-06-08 15:43:15 +0530

    Upload files

---
+++ index.js
@@ -0,0 +1,67 @@
+
+// Detect button click
+var numberOfDrums = document.querySelectorAll(".drum").length;
+for(var i=0; i<numberOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
+    });
+}
+
+// Detect keyboard press
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w": 
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        
+        case "a": 
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+            
+        case "s": 
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+
+        case "d": 
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        case "j": 
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+        case "k": 
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        
+        case "l": 
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+    
+        default : console.log(key);
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var ele = document.querySelector("." + currentKey)
+    ele.classList.add("pressed");
+
+    setTimeout(function() {
+        ele.classList.remove("pressed");
+    }, 100);
+}
